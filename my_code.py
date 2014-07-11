9cd565eb6c4cd2983c10c17c354b3266b327b675
morentharia <mor.entharia@gmail.com>
2014-07-11 17:47:24 +0400

    пиздец тут ад

--- my_code.py
+++ my_code.py
@@ -1,20 +1,74 @@
-import tornado.ioloop
-import pyrestful.rest
+# -*- encoding: utf-8 -*-
+import api
+import serial
+import kkmdrv
+import re
+import settings
 
-from pyrestful import mediatypes
-from pyrestful.rest import get
+if 1:
+    func_name = 'hello'
+    data = {
+        'name': 'zoi',
+        'address': 'hahaha',
+    }
+
+    if hasattr(api,func_name):
+        print getattr(api, func_name)(**data)
 
 
-class EchoService(pyrestful.rest.RestHandler):
-    @get(_path="/echo/{name}", _produces=mediatypes.APPLICATION_JSON)
-    def sayHello(self, name):
-        return {"Hello": name}
+    ser = serial.Serial(
+        "/dev/tty.fiscal", 115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        timeout=1.7,
+        writeTimeout=1.7,
+    )
+    sfrk = kkmdrv.ShtrihFRK(kkmdrv.DEFAULT_PASSWORD,
+                            kkmdrv.DEFAULT_ADM_PASSWORD,
+                            ser)
 
-if __name__ == '__main__':
-    try:
-        print "Start the echo service"
-        app = pyrestful.rest.RestService([EchoService])
-        app.listen(8080)
-        tornado.ioloop.IOLoop.instance().start()
-    except KeyboardInterrupt:
-        print "\nStop the echo service"
+    # print sfrk.getStatusString()
+    print sfrk.kkm.cashIncome(234.0)
+
+
+def camel_to_underscore(name):
+    name = name[0].lower() + name[1:]
+    camel_pat = re.compile(r'([A-Z])')
+    return camel_pat.sub(lambda x: '_' + x.group(1).lower(), name)
+
+def underscore_to_camel(name):
+    under_pat = re.compile(r'_([a-z])')
+    return under_pat.sub(lambda x: x.group(1).upper(), name)
+
+print camel_to_underscore('ReportWClose')
+print underscore_to_camel('report_w_close')
+
+
+if 0:
+    import kkm
+    kkmDev = kkm.Atol.AtolKKM(
+        {
+            'port': '/dev/tty.fiscal',
+            'baudrate': 115200
+        }
+    )
+
+    # name = 'dfjdkf'
+    # price = 3434.0
+    # count = 33.0
+
+
+    kkmDev.OpenCheck()
+    # # kkmDev.Sell(name.strip(),
+    # #             Decimal(price.strip()),
+    # #             Decimal(count.strip()),
+    # #             0)
+    # # kkmDev.Discount(discount, kkm.kkm_Check_dis)
+    # kkmDev.Payment(payment)
+
+
+    kkmDev.PrintToDisplay('\x0C')
+    kkmDev.PrintToDisplay('')
+    max = kkmDev.getDisplayStringMax()
+    kkmDev.PrintToDisplay('Спасибо'.center(max))
+    kkmDev.PrintToDisplay('за покупку!'.center(max))
--- web.py
+++ web.py
@@ -7,17 +7,80 @@ import tornado.web
 import json
 import traceback
 import serial
-import kkmdrv
 import logging
+import re
+
+import kkmdrv
 import settings
+import api
+
 
 PASSWORD = "1231231qweqweqwe"
+
+
+class MyKKMHandler(tornado.web.RequestHandler):
+    def post(self, path):
+        self.add_header("Content-Type", "application/json; charset=utf-8")
+        try:
+            logging.info(path)
+            logging.info(self.request.body)
+
+            route = {
+                "check/print/": lambda summ: api.kkm.cashIncome(float(summ)),
+                "check/cancel/": api.kkm.cashOutcome,
+                "report/": api.print_report,
+                # "cash/income/",
+                # "cash/outcome/",
+                # "drawer/open/",
+            }
+
+            func_name = path
+            if path in route:
+                func = route[path]
+            elif hasattr(api, func_name):
+                func = getattr(api, path)
+            elif hasattr(api.kkm, self._underscore_to_camel(func_name)):
+                func = getattr(api, path)
+            else:
+                raise StandardError('No such method {}'.format(func_name))
+
+            data = json.loads(self.request.body)
+            func(**data)
+        except Exception as e:
+            traceback.print_exc()
+            self.error(msg=str(e))
+        else:
+            self.success()
+        finally:
+            self.finish()
+
+    def error(self, msg=None):
+        error = {"success": False}
+        if msg is not None:
+            error["error"] = msg
+        self.__write(error)
+
+    def success(self):
+        self.__write({"success": True})
+
+    def __write(self, dict={}):
+        self.write(json.dumps(dict))
+
+    def _camel_to_underscore(self, name):
+        name = name[0].lower() + name[1:]
+        camel_pat = re.compile(r'([A-Z])')
+        return camel_pat.sub(lambda x: '_' + x.group(1).lower(), name)
+
+    def _underscore_to_camel(self, name):
+        under_pat = re.compile(r'_([a-z])')
+        return under_pat.sub(lambda x: x.group(1).upper(), name)
 
 
 class KKMHandler(tornado.web.RequestHandler):
 
     def initialize(self):
         self.kkm = self.application.kkm
+        api.kkm = self.application.kkm
 
     def prepare(self):
         password = self.get_argument("password", default=None)
@@ -26,8 +89,6 @@ class KKMHandler(tornado.web.RequestHandler):
 
     def post(self):
         self.add_header("Content-Type", "application/json; charset=utf-8")
-        self.write(json.dumps(dict))
-        return
         try:
             print self.request.body
             data = json.loads(self.request.body)
@@ -117,15 +178,19 @@ class CancelCheck(KKMHandler):
         self.kkm.cancelCheck()
 
 
+
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
+            # (r"/api2/([^/]+)/?", My),
+            (r"/api2/(.*$)", MyKKMHandler),
             (r"/api/check/print/", PrintCheck),
             (r"/api/check/cancel/", CancelCheck),
             (r"/api/report/", PrintReport),
             (r"/api/cash/income/", CashIncome),
             (r"/api/cash/outcome/", CashOutcome),
             (r"/api/drawer/open/", OpenCashDrawer),
+
         ]
         conf = dict(
             template_path='templates',
@@ -134,10 +199,11 @@ class Application(tornado.web.Application):
             autoreload=True,
         )
 
-        if conf['debug']:
+        if 0:
             import mock
             self.kkm = mock.MagicMock()
-        else:
+
+        if 1:
             ser = serial.Serial(
                 "/dev/tty.fiscal", 115200,
                 parity=serial.PARITY_NONE,
@@ -148,10 +214,16 @@ class Application(tornado.web.Application):
             sfrk = kkmdrv.ShtrihFRK(kkmdrv.DEFAULT_PASSWORD,
                                     kkmdrv.DEFAULT_ADM_PASSWORD,
                                     ser)
-            sfrk.kkm.reportWClose(kkmdrv.DEFAULT_ADM_PASSWORD)
-            self.kkm = sfrk.kkm
+            # sfrk.kkm.reportWClose(kkmdrv.DEFAULT_ADM_PASSWORD)
+
+            api.kkm = sfrk.kkm
 
         tornado.web.Application.__init__(self, handlers, **conf)
+
+
+
+
+
 
 
 if __name__ == "__main__":
